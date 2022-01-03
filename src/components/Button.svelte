10b9b39cf124ddc9726eab8e9b52d15f2ae53bcf
opensas <opensas@gmail.com>
2022-01-03 12:22:29 -0300

    initial commit

---
+++ src/components/Button.svelte
@@ -0,0 +1,7 @@
+<script>
+	let className = '';
+	export { className as class };
+	export let label = 'Click me!';
+</script>
+
+<button class="bg-red-500 {className}">{label}</button>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+
+import Button from "/src/components/Button.svelte";
+
+</script>
+<h1 class="bg-green-800= bg-blue-400">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button class="bg-blue-200"/>
